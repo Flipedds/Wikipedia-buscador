2c16daf9e3a80e527baf4957a283f96e02ce74ca
Filipe André <110608654+Flipedds@users.noreply.github.com>
2022-10-30 18:40:12 +0000

    Tratamento de erros

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import wikipedia
-
-pesquisa = input ("o que você deseja saber?")
-wikipedia.set_lang("pt")
-result = wikipedia.summary(pesquisa)
-
-salvar_arquivo = input("qual nome deseja dar ao arquivo")
-salvar_arquivo += ".txt"
-
-arquivo = open(salvar_arquivo,'w')
-arquivo.write(result) 
-arquivo.close() 
+try:
+	pesquisa = input ("o que você deseja saber?\n")
+	wikipedia.set_lang("pt")
+	result = wikipedia.summary(pesquisa)
+	salvar_arquivo = input("qual nome deseja dar ao arquivo:\n")
+	salvar_arquivo += ".txt"
+	arquivo = open(salvar_arquivo,'w')
+	arquivo.write(result) 
+	arquivo.close()
+except:
+	print("não foi possível encontrar nada na wikipedia, tente novamente")
